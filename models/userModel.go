c77cf8d265c2bcc49e07362bc9470c50701e75d2
vatsal <vatsaleduc@gmail.com>
2025-03-06 14:34:23 +0530

    removed spaces from struct definitions

--- models/userModel.go
+++ models/userModel.go
@@ -8,10 +8,10 @@ import (
 
 type User struct {
 	ID            primitive.ObjectID `bson:"_id"`
-	First_Name    *string            `json:"first_name" validate:"required, min=2, max=100"`
-	Last_Name     *string            `json:"last_name" validate:"required, min=2, max=100"`
-	Password      *string            `json:"password" validate:"required, min=6"`
-	Email         *string            `json:"email" validate:"email, required"`
+	First_Name    *string            `json:"first_name" validate:"required,min=2,max=100"`
+	Last_Name     *string            `json:"last_name" validate:"required,min=2,max=100"`
+	Password      *string            `json:"password" validate:"required,min=6"`
+	Email         *string            `json:"email" validate:"email,required"`
 	Phone         *string            `json:"phone" validate:"required"`
 	Token         *string            `json:"token"`
 	User_Type     *string            `json:"user_type" validate:"required, eq=ADMIN|eq=USER"`
