be3d600a440f7b22818985caa4a6819b7e217649
vatsal <vatsaleduc@gmail.com>
2025-03-06 11:23:14 +0530

    login and password handler functions completed

--- controllers/userController.go
+++ controllers/userController.go
@@ -15,17 +15,31 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 var validate = validator.New()
 
-func HashPassword() {
-
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
 }
 
-func VerifyPassword() {
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
 
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("email or password is incorrect")
+		check = false
+	}
+	return check, msg
 }
 
 func SignUpUser() gin.HandlerFunc {
@@ -54,6 +68,8 @@ func SignUpUser() gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the email"})
 		}
 
+		password := HashPassword(*user.Password)
+		user.Password = &password
 		countPhone, errPhone := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
 		defer cancel()
 		if errPhone != nil {
@@ -84,7 +100,46 @@ func SignUpUser() gin.HandlerFunc {
 	}
 }
 
-func LoginUser() {
+func LoginUser() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background())
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&user)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "email or password is incorrect"})
+			return
+		}
+
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+
+		if passwordIsValid != true {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		if foundUser.Email != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user not found"})
+		}
+		token, refreshToken, _ := helpers.GenerateAllTokens(*foundUser.Email, *foundUser.First_Name, *foundUser.Last_Name, *foundUser.User_Type, *&foundUser.User_ID)
+		helpers.UpdateAllTokens(token, refreshToken, foundUser.User_ID)
+		err = userCollection.FindOne(ctx, bson.M{"user_id": foundUser.User_ID}).Decode(&foundUser)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		c.JSON(http.StatusOK, foundUser)
+	}
 
 }
 
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -3,12 +3,11 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	controllers "github.com/jain-vatsal/go-jwt/controllers"
-	middleware "github.com/jain-vatsal/go-jwt/middleware"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.Use(middleware.AuthenticateUser())
+	// incomingRoutes.Use(middleware.AuthenticateUser())
 
-	incomingRoutes.GET("/users", controllers.GetAllUsers())
+	// incomingRoutes.GET("/users", controllers.GetAllUsers())
 	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
 }
